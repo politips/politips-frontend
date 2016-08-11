9b9782bd43bc4275098eebc65d8676e0cd0a1c55
Javad Karabi <karabijavad@gmail.com>
2016-08-11 05:13:54 +0000

    wip

--- src/app.jsx
+++ src/app.jsx
@@ -2,52 +2,16 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link, browserHistory } from 'react-router'
 
-const App = React.createClass({/*...*/})
-const About = React.createClass({/*...*/})
-const NoMatch = React.createClass({/*...*/})
-
-const Users = React.createClass({
+const App = React.createClass({
   render() {
+    console.log('abc');
+    console.log('abc');
     return (
-      <div>
-        <h1>Users</h1>
-        <div className="master">
-          <ul>
-            {/* use Link to route around the app */}
-            {this.state.users.map(user => (
-              <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
-            ))}
-          </ul>
-        </div>
-        <div className="detail">
-          {this.props.children}
-        </div>
-      </div>
+      <div>xxx</div>
     )
   }
 })
 
-const User = React.createClass({
-  componentDidMount() {
-    this.setState({
-      // route components are rendered with useful information, like URL params
-      user: findUserById(this.props.params.userId)
-    })
-  },
-
-  render() {
-    return (
-      <div>
-        <h2>{this.state.user.name}</h2>
-        {/* etc. */}
-      </div>
-    )
-  }
-})
-
-// Declarative route configuration (could also load this config lazily
-// instead, all you really need is a single root route, you don't need to
-// colocate the entire config).
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
