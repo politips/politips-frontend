f81313a99a0b679016da845b160bea77b0e85987
Javad Karabi <karabijavad@gmail.com>
2016-08-11 05:37:21 +0000

    wip

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,10 @@
 import './app.scss';
 
-import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link, browserHistory } from 'react-router'
 
-import About from './about';
-
-const App = React.createClass({
-  render() {
-    return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-12'>
-            {this.props.children}
-          </div>
-        </div>
-      </div>
-    )
-  }
-})
+import App from './components/app';
+import About from './components/about';
 
 render((
   <Router history={browserHistory}>
---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = React.createClass({
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+
