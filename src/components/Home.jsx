d6c89d44e4f2bb3d7ff9307950e5bbae324e6fe5
Javad Karabi <karabijavad@gmail.com>
2016-08-12 01:43:31 -0500

    wip

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = React.createClass({
+  render() {
+    return (
+      <div>
+        home
+      </div>
+    )
+  }
+})
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { Link } from 'react-router'
+import { Navbar as RBNavbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import { logout } from '../actions';
+
+const Navbar = React.createClass({
+  render() {
+    return (
+      <RBNavbar inverse>
+        <RBNavbar.Header>
+          <RBNavbar.Brand>
+            <Link to={'/'}>Politips</Link>
+          </RBNavbar.Brand>
+        </RBNavbar.Header>
+        <RBNavbar.Collapse>
+          <Nav>
+            <LinkContainer to={'/about/'}>
+              <NavItem>About</NavItem>
+            </LinkContainer>
+          </Nav>
+          <Nav pullRight>
+            <NavDropdown id="accountDropdown" title={this.props.user ? this.props.user.username : 'My Account'}>
+            { this.props.user ? (
+              <MenuItem onClick={this.props.logout}>Logout</MenuItem>
+            ) : (
+              <LinkContainer to={'/login/'}>
+                <MenuItem>Login</MenuItem>
+              </LinkContainer>
+            ) }
+            </NavDropdown>
+          </Nav>
+        </RBNavbar.Collapse>
+      </RBNavbar>
+    )
+  }
+})
+
+const mapStateToProps = (state) => {
+  return state.auth
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: function() {
+      return dispatch(logout());
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
